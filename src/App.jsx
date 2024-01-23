76fce725a70fc16b670c641d1f8c5aa5ced594a7
Bhawesh Choudhary <114644299+CaptainShaqX@users.noreply.github.com>
2024-01-23 12:37:27 +0000

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Bhawesh Choudhary",
+  title: "Web Designer | Coder | Learner",
+  email: "bhawesh.choudhary@studentambassadors.com",
+  gitHub: "https://github.com/CaptainShaqX",
+  instagram: "captain_shaq",
+  linkedIn: "Bhawesh Choudhary",
+  medium: "Bhawesh Choudhary",
+  twitter: "Captain_Shaq_",
 };
 
 const primaryColor = "#4E567E";
